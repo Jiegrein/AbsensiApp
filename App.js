237b0c65beccf66cc7304aa0480fdb40f1a1a5f8
DANIEL ABEDNEGO <daniel.abednego@project.accelist.com>
2021-11-30 15:56:37 +0700

    qr camera working reasonably fast

--- App.js
+++ App.js
@@ -1,98 +1,151 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
-import { useEffect, useState } from 'react';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { Camera } from 'expo-camera';
+
+// export default function App() {
+//   const [hasPermission, setHasPermission] = useState(null);
+//   const [scanned, setScanned] = useState(false);
+//   const [text, setText] = useState('Not yet scanned')
+
+//   const askForCameraPermission = () => {
+//     (async () => {
+//       const { status } = await BarCodeScanner.requestPermissionsAsync();
+//       setHasPermission(status === 'granted');
+//     })()
+//   }
+
+//   // Request Camera Permission
+//   useEffect(() => {
+//     askForCameraPermission();
+//   }, []);
+
+//   // What happens when we scan the bar code
+//   const handleBarCodeScanned = ({ type, data }) => {
+//     setScanned(true);
+//     setText(data)
+//     console.log('Type: ' + type + '\nData: ' + data)
+//   };
+
+//   // Check permissions and return the screens
+//   if (hasPermission === null) {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Requesting for camera permission</Text>
+//       </View>)
+//   }
+//   if (hasPermission === false) {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={{ margin: 10 }}>No access to camera</Text>
+//         <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+//       </View>)
+//   }
+
+//   // Return the View
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.barcodebox}>
+//         <BarCodeScanner
+//           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+//           style={{ height: 400, width: 400 }} />
+//       </View>
+//       <Text style={styles.maintext}>{text}</Text>
+
+//       {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+//     </View>
+//   );
+// }
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not Yet Scanned');
+  const [text, setText] = useState('Not yet scanned');
 
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
-  }
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 
-  //Request Camera Permision
   useEffect(() => {
-    askForCameraPermission();
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
   }, []);
 
-  //What happens when we scan the bar code
-  const handleBarcodeScanned = ({type},{data}) =>{
-    setScanned(true);
-    setText(data);
-    console.log('Type: ' +type + '\nData' +data);
+  if (hasPermission === null) {
+    return <View />;
   }
-
-  //Check permissions and return the screens
-  if(hasPermission === null){
-    return (
-      <View style={styles.container}>
-        <Text>Camera Permision Dood</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
-
-  else if(hasPermission === false){
-    return (
-      <View style={styles.container}>
-        <Text>No permission dood Pepehands</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()}/>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-
-  else if(hasPermission === true){
-    return (
-      <View style={styles.container}>
-      <View style={styles.barcodeBox}>
-        <BarCodeScanner
-          onBarcodeScanned={handleBarcodeScanned}
-          barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-          style={{ height: 400, width: 400 }} />
-      </View>
-      <Text style={styles.maintext}>{text}</Text>
-
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
-    </View>
-    );
-  }
-  
   return (
     <View style={styles.container}>
-      <Text>Dood</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+
+    {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
     </View>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   maintext: {
+//     fontSize: 16,
+//     margin: 20,
+//   },
+//   barcodebox: {
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     height: 300,
+//     width: 300,
+//     overflow: 'hidden',
+//     borderRadius: 30,
+//     backgroundColor: 'tomato'
+//   }
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-
-  barcodeBox: {    
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 300,
-    width: 300,
-    overflow: 'hidden',
-    borderRadius: 30,
-    backgroundColor: 'red'
+  camera: {
+    flex: 1,
   },
-
-  maintext:{
-    fontSize: 16,
-    margin: 20
-  }
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
 });
